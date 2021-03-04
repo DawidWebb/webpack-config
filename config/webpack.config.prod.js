cd8c89d9a25dc1faee173195207a1c9c74a49d94
DawidWebb <tomasz.matras@developerweb.pl>
2021-03-04 20:15:01 +0100

    final setup

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -2,6 +2,7 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -52,11 +53,19 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "nowa aplikacja",
+      title: "new app",
       template: "src/templates/template.html",
     }),
     new MiniCssExtractPlugin({
       filename: "[name]-[contenthash:6].css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public/images",
+          to: "images",
+        },
+      ],
     }),
   ],
 };
